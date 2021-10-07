ddf4fe28f3fc32c018e29146c2f7630865dce651
virupakshaK <virupaksha579@gmail.com>
2021-10-07 11:01:25 +0530

    cleaning userpinservice file

--- src/main/java/com/docomo/pinmanager/services/UserPinService.java
+++ src/main/java/com/docomo/pinmanager/services/UserPinService.java
@@ -1,22 +1,23 @@
 /**
- *
+ * 
  */
 package com.docomo.pinmanager.services;
 
-import com.docomo.pinmanager.dto.Message;
-import com.docomo.pinmanager.dto.UserPinDTO;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.docomo.pinmanager.dto.Message;
+import com.docomo.pinmanager.dto.UserPinDTO;
 import com.docomo.pinmanager.entities.UserPin;
 import com.docomo.pinmanager.exceptions.UserPinNotFoundException;
 import com.docomo.pinmanager.repositories.PinManageRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 /**
  * @author virupaksha.kuruva
@@ -24,47 +25,42 @@ import org.slf4j.LoggerFactory;
  */
 @Service
 public class UserPinService {
+	 private static final Logger logger = LoggerFactory.getLogger(UserPinService.class);
+	@Autowired
+	PinManageRepository pinManageRepository;
 
-    @Autowired
-    PinManageRepository pinManageRepository;
-    private static final Logger logger = LoggerFactory.getLogger(UserPinService.class);
+	public UserPinDTO generateUserPin(String MSISDN) {
+		int pin = 0;
 
-    public UserPinDTO generateUserPin(String MSISDN) {
-        int pin = 0;
+		Random random = new Random();
+		pin = random.nextInt(10000);
 
-        Random random = new Random();
-        pin = random.nextInt(10000);
+		UserPin userPin = new UserPin();
+		userPin.setMsisdn(MSISDN);
+		userPin.setPin(String.format("%04d", pin));
+		userPin.setPinVaidatedStatus(false);
+		userPin.setBadTryCount(0);
+		userPin.setPinMaxInvalidCount(3);
+		userPin.setPinRemovalInHour(1);
+		userPin.setCreated_date(new Date());
+		userPin.setPinBlokedStatus(0);
+		logger.debug("Pin is generated for MSISDN:" + MSISDN);
+		pinManageRepository.save(userPin);
+		logger.debug("Pin is store in the DB for MSISDN:" + MSISDN);
+		if (pin > 0) {
+			logger.debug("Pin is generated for the given MSISDN:" + pin);
+		} else {
+			logger.debug("Pin is not generated for the given MSISDN:" + pin + ", somethig is wrong in service layer");
+		}
+		UserPinDTO userPinDTO = new UserPinDTO();
+		userPinDTO.setPin(String.format("%04d", pin));
+		userPinDTO.setMsisdn(MSISDN);
+		return userPinDTO;
+	}
 
-        UserPin userPin = new UserPin();
-        userPin.setMsisdn(MSISDN);
-        userPin.setPin(String.format("%04d", pin));
-        userPin.setPinVaidatedStatus(false);
-        userPin.setBadTryCount(0);
-        userPin.setPinMaxInvalidCount(3);
-        userPin.setPinRemovalInHour(1);
-        userPin.setCreated_date(new Date());
-        userPin.setPinBlokedStatus(0);
-        logger.debug("Pin is generated for MSISDN:" + MSISDN);
-        pinManageRepository.save(userPin);
-        logger.debug("Pin is store in the DB for MSISDN:" + MSISDN);
-
-        if (pin > 0) {
-            logger.debug("Pin is generated for the given MSISDN:" + pin);
-        } else {
-            logger.debug("Pin is not generated for the given MSISDN:" + pin + ", somethig is wrong in service layer");
-        }
-
-        UserPinDTO userPinDTO = new UserPinDTO();
-        userPinDTO.setPin(String.format("%04d", pin));
-        userPinDTO.setMsisdn(MSISDN);
-
-        return userPinDTO;
-    }
-
-    @SuppressWarnings("unused")
-    public Message validateUserPin(UserPinDTO passedUserPinData) {
-
-        Message message = new Message();
+	@SuppressWarnings("unused")
+	public Message validateUserPin(UserPinDTO passedUserPinData) {
+		Message message = new Message();
 
         UserPin userPin = pinManageRepository.getById(passedUserPinData.getMsisdn());
         if (isMsisdnExistInDB(userPin, passedUserPinData)) {
@@ -77,7 +73,7 @@ public class UserPinService {
                 return message;
             } else {
                 if (userPin.getPinBlokedStatus() == 0) {
-                    logger.debug("given pin:" + passedUserPinData.getPin() + " stored pin: " + userPin.getPin());
+                    //logger.debug("given pin:" + passedUserPinData.getPin() + " stored pin: " + userPin.getPin());
                     if (validatePinAgainstDB(userPin, passedUserPinData, message)) {
                         return message;
                     } else {
@@ -90,8 +86,17 @@ public class UserPinService {
             }
         }
         return message;
-    }
+			}
 
+/*	public UserPin getUserPinByMSISDN(String MSISDN) {
+
+		return pinManageRepository.getById(MSISDN);
+	}
+
+	public boolean updatePinValidationStatus(UserPin user) {
+		return false;
+	}*/
+	
     private boolean validatePinAgainstDB(UserPin userPin, UserPinDTO passedUserPinData, Message message) {
         if (userPin.getPin().equals(passedUserPinData.getPin())) {
             // pin matched updating in DB
@@ -106,7 +111,7 @@ public class UserPinService {
             return false;
         }
     }
-
+	
     private void updateFailedStatusInDB(UserPin userPin, Message message) {
         int invalidCount = userPin.getBadTryCount();
         if (invalidCount + 1 > userPin.getPinMaxInvalidCount()) {
@@ -123,16 +128,7 @@ public class UserPinService {
         }
         pinManageRepository.save(userPin);
     }
-
-    public UserPin getUserPinByMSISDN(String MSISDN) {
-
-        return pinManageRepository.getById(MSISDN);
-    }
-
-    public boolean updatePinValidationStatus(UserPin user) {
-        return false;
-    }
-
+	
     public boolean isPinExpired(UserPin userPin, UserPinDTO passedUserPinData) {
 
         boolean valid = false;
@@ -145,23 +141,23 @@ public class UserPinService {
             cal.setTime(currentDate);
             cal.add(Calendar.HOUR, -userPin.getPinRemovalInHour());
             Date oneHourBack = cal.getTime();
-            valid =   valid = oneHourBack.before(userPin.getCreated_date());
+            valid = oneHourBack.before(userPin.getCreated_date());
 
             // check validated Status
             return valid;
         }
         return false;
     }
+	
+	   public boolean isMsisdnExistInDB(UserPin userPin, UserPinDTO passedUserPinData) {
 
-    public boolean isMsisdnExistInDB(UserPin userPin, UserPinDTO passedUserPinData) {
+	        if (userPin != null) {
+	            return true;
+	        } else {
+	            logger.debug("User pin not found in DB for the given MSISDN: " + passedUserPinData.getMsisdn());
+	            throw new UserPinNotFoundException(404, "User pin not found for give MSISDN.");
+	        }
 
-        if (userPin != null) {
-            return true;
-        } else {
-            logger.debug("User pin not found in DB for the given MSISDN: " + passedUserPinData.getMsisdn());
-            throw new UserPinNotFoundException(404, "User pin not found for give MSISDN.");
-        }
-
-    }
+	    }
 
 }
